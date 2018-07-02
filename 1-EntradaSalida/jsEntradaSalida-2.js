e180b103d1c70d1246ee1c985d73e8e69069045d
german221 <40763536+german221@users.noreply.github.com>
2018-07-02 12:06:20 -0300

    ejercicios 1,2 y 3
    primera clase

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,17 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	//var person = prompt("Please enter your name", "German Guerrero");
+/*sssss
+sssssss
+*/
+
+//primer intento 02/07/18
+
+var nombre1
+nombre1=prompt("ingrese su nombre", "Ingrese AQUI su nombre")
+alert("Usted ingreso: "+nombre1+", Es esto correcto?")
+alert("Felicidades usted ha finalizado el ejercicio!")
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,19 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+
+/*
+alert("Ejercicio 3");
+console.log("hola que tal?")	
+*/
+
+/*primer intento
+document.getElementById('')
+*/
+
+//segundo intento
+var nombre1;
+nombre1=document.getElementById('elNombre').value;
+alert(nombre1);
 }
-
-
