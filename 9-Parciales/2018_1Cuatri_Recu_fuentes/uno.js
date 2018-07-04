19ee321ff3bd22e205e3f32be0b3e5576bcfaed6
GermanGuerrero <40763536+german221@users.noreply.github.com>
2018-07-04 11:59:04 -0300

    ejercicios 1-2
    mis intentos

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,18 @@
 
-function mostrar()
+function Mostrar()
 {
+var altura;
+var base;
+var superficie;
+var perimetro;
 
+altura=prompt("ingrese la altura");
+altura=parseInt(altura);
+base=prompt("ingrese la base");
+base=parseInt(base);
+
+superficie=(altura*base)/2;
+
+perimetro= (base*3);
+alert("la superficie es: "+superficie+" y el perimetro es "+perimetro);
 }
