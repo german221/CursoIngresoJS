62b1ddfd7b94193fa103b1724fedfbafd32debd1
GermanGuerrero <40763536+german221@users.noreply.github.com>
2018-07-04 12:22:48 -0300

    ejercicio 3
    ejercicio 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,30 @@
-function mostrar()
+function Mostrar()
 {
+var precio;
+var porcent;
+var precioDes;
+var descuento;
+var iva;
+var precioIva
+var precioIvad;
+
+precio=prompt("Ingrese el precio");
+precio=parseInt(precio);
+porcent=prompt("Ingrese el porcentaje de descuento");
+porcent=parseInt(porcent);
+iva=prompt("Ingrese el IVA");
+
+descuento=precio*(porcent/100);
+
+precioDes=precio*(porcent/100)+precio;
+
+precioIva=(precio*iva%)-precio;
+precioIvad=(descuento*iva%)-descuento;
+
+alert("El descuento en dinero es "+descuento);
+alert("El precio con el descuento es "+precioDes);
+alert("El Iva del descuento es "+precioIvad+" El iva del precio total es "+precioIva);
+
+
 
 }
