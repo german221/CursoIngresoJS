904df8640bf12a13328915aecc1dbff23f32b802
German Guerrero <40763536+germandgm@users.noreply.github.com>
2018-08-05 19:20:20 -0300

    practica parcial
    intento 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,110 @@
+/*
+Testeo con los siguientes datos 
+( d , -150(mal) , -50(bien)) 
+( z , 0) 
+( g , 20) 
+( a , 150(mal) , 0(bien)) 
+( b , 3) 
+( k , 7)
+
+a) La cantidad de números pares. 
+b) La cantidad de números impares. 
+c) La cantidad de ceros. 
+d) El promedio de todos los números positivos ingresados. 
+e) La suma de todos los números negativos.
+f) El número y la letra del máximo y el mínimo.
+
+*/
+
+
 function mostrar()
 {
+    var letra;
+    var numero;
+    var cPares;
+    var cImpares;
+    var cCeros;
+    var acumNumeros;
+    var acumNegativos;
+    var respuesta;
+    var i;
+    var cPositivos;
+    var promedio;
 
+    i=0;
+    cPositivos=0;
+    cCeros=0;
+    cPares=0;
+    cImpares=0;
+    acumNumeros=0;
+    acumNegativos=0;
+    respuesta='s';
+
+while(respuesta!='n')
+  {
+    letra=prompt("Ingrese una letra a-z");
+    numero=parseInt(prompt("Ingrese un numero -100 hasta 100 : "));
+    while(numero<-100 || numero >100)
+    {
+        numero=parseInt(prompt("Error ingrese un numero valido -100 hasta 100"));
+    }
+
+    if(numero/2==0)
+    {
+        cPares++;
+    }
+    else
+    {
+        cImpares++;
+    }
+
+    if(numero==0)
+    {
+        cCeros++;
+    }
+
+    if(numero>0)
+    {
+        acumNumeros=acumNumeros+numero;
+        cPositivos++;
+    }
+    
+
+    if(numero<0)
+    {
+        acumNegativos=acumNegativos+numero;
+    }
+
+    if(i==0)
+    {
+        max=numero;
+        letraMax=letra;
+        min=numero;
+        letraMin=letra;
+        i++;
+    }
+    if(numero<min)
+    {
+        min=numero;
+        letraMin=letra;
+    }
+    if(numero>max)
+    {
+        max=numero;
+        letraMax=letra;
+    }
+    respuesta=prompt("desea ingresar mas datos? ");
 }
+
+  promedio=acumNumeros/cPositivos;
+
+  document.write("cantidad de pares: "+cPares+"<br>");
+  document.write("cantidad de impares: "+cImpares+"<br>");
+  document.write("cantidad de ceros: "+cCeros+"<br>");
+  document.write("promedio numeros positivos: "+promedio+"<br>");
+  document.write("suma negativos: "+acumNegativos+"<br>");
+  document.write("letra del maximo: "+letraMax+" numero maximo: "+max+ "<br>");
+  document.write("letra del maximo: "+letraMin+" numero maximo: "+min+ "<br>");
+}
+
+
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,67 @@
 function mostrar()
 {
+ var nota;
+ var sexo;
+ var contador;
+ var acuNotas;
+ var promedio;
+ var min;
+ var max;
+ var i;
+ var sexoNotabaja;
+ var cNotas;
+
+ contador=1;
+ acuNotas=0;
+ i=0;
+ cNotas=0;
+
+ while (contador<=5)
+ {  
+    nota=parseInt(prompt("ingrese la nota del alumno: "));
+    while(nota<0 || nota >10)
+    {
+        nota=prompt("nota invalida, ingrese nota entre 0-10 ");
+    }
+
+    acuNotas=acuNotas+nota;
+    
+    sexo=prompt("ingrese el sexo f/m");
+    while(sexo!="f" && sexo !="m")
+    {
+        sexo=prompt("sexo invalido, ingrese f/m ");
+    }
+
+    promedio = acuNotas / 5;
+
+    if(i==0)
+    {
+        min=nota;
+        sexoNotabaja=sexo;
+        max=nota;
+        
+        i++;
+    }
+    if(nota<min)
+    {
+        min=nota;
+        sexoNotabaja=sexo;
+    }
+    if(nota>max)
+    {
+        max=nota;
+    }
+    if(sexo=="m" && nota>=6)
+    {
+        cNotas++;
+    }
+        
+    contador++;
+
+ }
+
+ alert("el promedio de las 5 notas es: "+promedio);
+ alert("la nota mas baja es: "+min+"y su sexo es: "+sexoNotabaja);
+ alert("cantidad de varones con nota mayor o igual a 6: "+cNotas);
 
 }
